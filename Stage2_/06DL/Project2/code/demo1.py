1969b7fe47e8b3f0985812c4ee7674a22aa05916
1wKo1sh1 <3250924358@qq.com>
2026-03-18 19:30:22 +0800

    蘑菇与邮件(高准确&高召回)

--- Stage2_/06DL/Project2/code/demo1.py
+++ Stage2_/06DL/Project2/code/demo1.py
@@ -1,70 +1,63 @@
-'''
-实例
-4601，其中垃圾1813，39.4%
-属性
-58，连续变量57，名义变量1（因变量）
-'''
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 import torch
 import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 
 # 定义默认为gpu
 torch.set_default_device('cuda')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-# 1.数据输入处理
-dataset = pd.read_csv('../dataset/spambase.data', header=None)
+# 1.数据输入处理(使用pyarrow引擎加速读取速度但是格式会变为独特格式后面转换出问题)
+dataset = pd.read_csv('../dataset/agaricus-lepiota.data', header=None, engine='c')
+dataset[0] = dataset[0].map({'e': True, 'p': False})
+print(dataset.head())
+dataset = pd.get_dummies(dataset,columns=list(range(1,23)))
+print(dataset.head()) # 8xxx x 118
 # 获取特征与分类
-X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
-# 划分
+X = dataset.iloc[:,1:].to_numpy()
+y = dataset.iloc[:,0].to_numpy()
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-# 标准化
-scaler = StandardScaler()
-x_train_scaled = scaler.fit_transform(x_train)# 学习参数保存到scaler对象，然后在转换
-x_test_scaled = scaler.transform(x_test)#使用学习过的参数进行转换
+# 标准化(原文为01已经不需要标准化)
+x_train_scaled = x_train
+x_test_scaled = x_test
 # 变为张量
 x_train_t = torch.tensor(x_train_scaled, dtype=torch.float32)
 x_test_t = torch.tensor(x_test_scaled, dtype=torch.float32)
 y_train_t = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
 y_test_t = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
 
-print('1')
 # 2.初始化神经元
-n = 57
-class ClassifySpamEmails(nn.Module):
+input_size = len(x_train_t[0])
+class MushroomsEdible(nn.Module):
     def __init__(self):
-        super(ClassifySpamEmails, self).__init__()
-        self.linear1 = nn.Linear(n, 1)
-        self.sigmoid = nn.Sigmoid()
+        super(MushroomsEdible, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 1),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        x = self.sigmoid(self.linear1(x))
-        return x
+        return self.layers(x)
 
-model = ClassifySpamEmails()
+model = MushroomsEdible()
 model.to(device)
-print('2')
-# 3.初始化损失函数优化器
+# 损失函数与优化器
 criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-print('3')
-# 4.迭代
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
+# 3.迭代过程
 # 初始化随机种子
 seed = 32
 torch.manual_seed(seed)
 # 定义一个小batch
 dataset = TensorDataset(x_train_t, y_train_t)
 # 将迭代器放到cuda上
-dataloader = DataLoader(dataset, batch_size=100, shuffle=True, generator=torch.Generator(device='cuda'))
-print('4')
+dataloader = DataLoader(dataset, batch_size=500, shuffle=True, generator=torch.Generator(device='cuda'))
 # 迭代次数
 epochs = 100
 for epoch in range(1,1+epochs):
@@ -83,12 +76,13 @@ for epoch in range(1,1+epochs):
     # 计算平均损失
     avg_loss = total_loss / len(dataloader)
     # 得到目前w和b(方便绘制)
-    w = model.linear1.weight.detach().cpu().numpy()
-    b = model.linear1.bias.detach().cpu().numpy()
+    w = model.layers[0].weight.detach().cpu().numpy()
+    b = model.layers[0].bias.detach().cpu().numpy()
 
     # 显示频率
     if epoch % 50 == 0 or epoch == 1:
-        print(f"epoches:{epoch}, loss:{avg_loss}")
+        print(f"epoches:{epoch}, loss:{avg_loss},\n目前w:{w}\nb:{b}")
+
 
 # 评估模型
 model.eval()
