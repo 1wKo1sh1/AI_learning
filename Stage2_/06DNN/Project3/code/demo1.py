82c37b2c91b73b351e367e575ec0f8d1e454a32b
1wKo1sh1 <3250924358@qq.com>
2026-03-20 20:20:52 +0800

    DNN项目

---
+++ Stage2_/06DNN/Project3/code/demo1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+# 定义默认为gpu
+torch.set_default_device('cuda')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# 1.数据输入处理
+# 有特殊繁体字符需要big5编码
+data = pd.read_csv("../dataset/train.csv", encoding="big5")
+print(data.head())
+# 前几列不需要
+data = data.iloc[:, 3:]
+# 缺失的NR元素改为0
+data[data=='NR'] = '0'
+data = data.astype(float)
+# 重新整理为一般的格式
+data = data.T
+print(data)
+# 一共24行、4320列，分离列，每18个为一组分为240组(相当此天的数据且每天24小时)
+total_blocks = 240
+rows_needed = total_blocks * 24
+cols_needed = 18
+df = pd.DataFrame(np.nan, index=range(rows_needed), columns=range(cols_needed))
+for i in range(0,4320+1-18,18):
+    print(i)
+    # n = i // 18
+    # df.iloc[24 * n : 24 * (n+1) , 0 : 19] = data.iloc[0 : 24 , i : 19 + i]
+    # print(df.head())
+
